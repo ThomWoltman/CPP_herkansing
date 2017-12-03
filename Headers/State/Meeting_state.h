1b949dcf5972c6146b15b9a99fbc071496f64361
Unknown <gwildt@avans.nl>
2017-12-03 16:50:10 +0100

    FIx

--- Headers/State/Meeting_state.h
+++ Headers/State/Meeting_state.h
@@ -5,4 +5,14 @@
 #ifndef CPP_HERKANSING_MEETING_STATE_H
 #define CPP_HERKANSING_MEETING_STATE_H
 
+#include "Game_state.h"
+
+class Meeting_state : public Game_state {
+public:
+    Meeting_state();
+    ~Meeting_state() override;
+    void run(Player &player, Game_state_context &context);
+private:
+};
+
 #endif //CPP_HERKANSING_MEETING_STATE_H
--- Source/State/Meeting_state.cpp
+++ Source/State/Meeting_state.cpp
@@ -2,4 +2,10 @@
 // Created by administrator on 3-12-17.
 //
 
-#include "Meeting_state.h"
+#include "../../Headers/State/Meeting_state.h"
+
+Meeting_state::Meeting_state() {}
+Meeting_state::~Meeting_state() {}
+void Meeting_state::run(Player &player, Game_state_context &context) {
+    cout<<"Meeting state";
+}
